64e4db7da6dc5d1e9ba5c74298d65a84a1c2c29e
abbyyvan <47904921+abbyyvan@users.noreply.github.com>
2024-11-11 20:18:11 -0800

    router with basename: my-website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 
+
 function App() {
   return (
-    // 设置 basename 为你的仓库名
-    <BrowserRouter basename="my-website">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
+    <BrowserRouter basename="/my-website">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
